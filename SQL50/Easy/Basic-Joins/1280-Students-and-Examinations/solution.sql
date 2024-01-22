35d328d119bed8b2b638a18317cbec6014d44d1c
Leo Zeng <qingchun1995@gmail.com>
2024-01-22 15:40:04 +0800

    docs: update solution code

--- SQL50/Easy/Basic-Joins/1280-Students-and-Examinations/solution.sql
+++ SQL50/Easy/Basic-Joins/1280-Students-and-Examinations/solution.sql
@@ -3,6 +3,6 @@ SELECT s.student_id, s.student_name, u.subject_name, COUNT(e.subject_name) AS at
 FROM students s
 CROSS JOIN subjects u
 LEFT JOIN examinations e
-ON e.student_id = s.student_id AND e.subject_name = u.subject_name
+ON e.subject_name = u.subject_name AND e.student_id = s.student_id
 GROUP BY 1, 2, 3
 ORDER BY 1, 3 ASC;
