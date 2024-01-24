f503461283fc3c23efde149d1821c5977b296906
Leo Zeng <qingchun1995@gmail.com>
2024-01-24 23:13:31 +0800

    docs: add a PostgreSQL solution to the question "1633. Percentage of Users Attended a Contest"

---
+++ SQL50/Easy/Basic-Aggregate-Functions/1633-Percentage-of-Users-Attended-a-Contest/solution.sql
@@ -0,0 +1,11 @@
+-- PostgreSQL - Solution 1 - Left Join
+SELECT contest_id, ROUND(COUNT(user_id)*100.0/num_users, 2) AS percentage
+FROM (
+    SELECT COUNT(DISTINCT u.user_id) AS num_users
+    FROM users u
+    LEFT JOIN register r
+    ON u.user_id = r.user_id
+)
+CROSS JOIN register
+GROUP BY 1, num_users
+ORDER BY 2 DESC, 1 ASC;
