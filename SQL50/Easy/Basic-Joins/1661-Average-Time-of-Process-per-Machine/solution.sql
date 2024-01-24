922d7018e8efd110faba680024698855a0088296
Leo Zeng <qingchun1995@gmail.com>
2024-01-24 10:02:44 +0800

    docs: add a PostgreSQL solution to the question "1661. Average Time of Process per Machine"

---
+++ SQL50/Easy/Basic-Joins/1661-Average-Time-of-Process-per-Machine/solution.sql
@@ -0,0 +1,18 @@
+-- PostgreSQL - Solution 1
+SELECT machine_id, ROUND(CAST((SUM(end_timestamp)-SUM(start_timestamp))/COUNT(process_id) AS numeric), 3) AS processing_time
+FROM (
+    SELECT machine_id, process_id,
+    MAX(CASE
+        WHEN activity_type = 'start' THEN timestamp
+        ELSE 0
+    END) AS start_timestamp,
+    MAX(CASE
+        WHEN activity_type = 'end' THEN timestamp
+        ELSE 0
+    END) AS end_timestamp
+    FROM activity
+    GROUP BY 1, 2
+    ORDER BY 1, 2
+)
+GROUP BY 1
+ORDER BY 2 ASC
