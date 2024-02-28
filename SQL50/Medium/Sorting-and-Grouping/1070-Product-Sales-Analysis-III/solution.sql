0aef30d4831f541b893134163d0df5cca5bfe395
Leo Zeng <qingchun1995@gmail.com>
2024-02-28 11:58:41 -0800

    add a different PostgreSQL solution to the question "1070. Product Sales Analysis III"

--- SQL50/Medium/Sorting-and-Grouping/1070-Product-Sales-Analysis-III/solution.sql
+++ SQL50/Medium/Sorting-and-Grouping/1070-Product-Sales-Analysis-III/solution.sql
@@ -5,3 +5,12 @@ FROM (
     FROM sales
 )
 WHERE year = first_year;
+
+-- PostgreSQL - Solution 2
+SELECT product_id, year AS first_year, quantity, price
+FROM sales
+WHERE (product_id, year) IN (
+    SELECT product_id, MIN(year)
+    FROM sales
+    GROUP BY 1
+)
