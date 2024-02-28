7ca85d405310118b1d9a8f89283d553a2b71ab4f
Leo Zeng <qingchun1995@gmail.com>
2024-02-27 16:21:45 -0800

    add a PostgreSQL solution to the question "1045. Customers Who Bought All Products"

---
+++ SQL50/Medium/Sorting-and-Grouping/1045-Customers-Who-Bought-All-Products/solution.sql
@@ -0,0 +1,14 @@
+-- PostgreSQL - Solution 1
+WITH t AS (
+    SELECT customer_id, COUNT(DISTINCT product_key) AS per_customer_total_products
+    FROM customer
+    GROUP BY 1
+    ORDER BY 1 ASC
+)
+SELECT t.customer_id
+FROM t
+JOIN (
+    SELECT COUNT(*) AS total_products
+    FROM product
+) f
+ON t.per_customer_total_products = f.total_products;
