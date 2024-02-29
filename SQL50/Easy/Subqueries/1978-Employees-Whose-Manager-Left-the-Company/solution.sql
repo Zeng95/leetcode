6f5afce0a5dd7d2ce1f0efa65c3836a4ff18363c
Leo Zeng <qingchun1995@gmail.com>
2024-02-29 09:13:41 -0800

    add a different PostgreSQL solution to the question "1978. Employees Whose Manager Left the Company"

--- SQL50/Easy/Subqueries/1978-Employees-Whose-Manager-Left-the-Company/solution.sql
+++ SQL50/Easy/Subqueries/1978-Employees-Whose-Manager-Left-the-Company/solution.sql
@@ -3,3 +3,14 @@ SELECT employee_id
 FROM employees
 WHERE salary < 30000 AND manager_id NOT IN (SELECT employee_id FROM employees)
 ORDER BY 1;
+
+-- PostgreSQL - Solution 2
+WITH etc AS (
+    SELECT employee_id, manager_id
+    FROM employees
+    WHERE salary < 30000
+)
+SELECT employee_id
+FROM etc
+WHERE manager_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY 1;
