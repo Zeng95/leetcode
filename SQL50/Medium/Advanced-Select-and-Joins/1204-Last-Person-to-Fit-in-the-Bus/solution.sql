271ea174908085de78fdd8e216cc7ca08160cbde
Leo Zeng <qingchun1995@gmail.com>
2024-03-02 15:42:27 -0800

    add a different PostgreSQL solution to the question "1204. Last Person to Fit in the Bus"

--- SQL50/Medium/Advanced-Select-and-Joins/1204-Last-Person-to-Fit-in-the-Bus/solution.sql
+++ SQL50/Medium/Advanced-Select-and-Joins/1204-Last-Person-to-Fit-in-the-Bus/solution.sql
@@ -8,3 +8,13 @@ FROM cte
 WHERE total_weight <= 1000
 ORDER BY total_weight DESC
 LIMIT 1;
+
+-- PostgreSQL - Solution 2
+SELECT a.person_name
+FROM queue a
+JOIN queue b
+ON a.turn >= b.turn
+GROUP BY 1
+HAVING SUM(b.weight) <= 1000
+ORDER BY SUM(b.weight) DESC
+LIMIT 1;
