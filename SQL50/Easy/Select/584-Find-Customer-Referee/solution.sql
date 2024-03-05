af912184850e202d4bf83ae98775783b122a5488
Leo Zeng <qingchun1995@gmail.com>
2024-03-04 16:20:27 -0800

    add the description of the question "584. Find Customer Referee"

--- SQL50/Easy/Select/584-Find-Customer-Referee/solution.sql
+++ SQL50/Easy/Select/584-Find-Customer-Referee/solution.sql
@@ -1,2 +1,14 @@
-SELECT name FROM customer WHERE referee_id != 2 OR referee_id IS NULL;
-SELECT name FROM customer WHERE COALESCE(referee_id, 0) <> 2;
+-- Solution 1
+SELECT name
+FROM customer
+WHERE referee_id != 2 OR referee_id IS NULL;
+
+-- Solution 2
+SELECT name
+FROM customer
+WHERE referee_id <> 2 OR referee_id = NULL;
+
+-- Solution 3
+SELECT name
+FROM customer
+WHERE COALESCE(referee_id, 0) <> 2;
