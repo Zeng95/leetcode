a365d21fd5ba1ac60a32be01f7dd15e022921955
Leo Zeng <qingchun1995@gmail.com>
2024-03-11 12:05:42 -0700

    add a different solution to the question "183. Customers Who Never Order"

--- Advanced SQL 50/Easy/Select/183-Customers-Who-Never-Order/solution.sql
+++ Advanced SQL 50/Easy/Select/183-Customers-Who-Never-Order/solution.sql
@@ -4,3 +4,11 @@ FROM customers c
 LEFT JOIN orders o
 ON c.id = o.customerId
 WHERE o.customerId IS NULL;
+
+-- PostgreSQL - Solution 2 - Filtering Data with Exclusion Criteria
+SELECT name AS Customers
+FROM customers c
+WHERE c.id NOT IN (
+    SELECT customerId
+    FROM orders
+);
