c963cbfef1480cec31ea447866c57d5221f7afe6
Leo Zeng <qingchun1995@gmail.com>
2024-03-12 11:33:39 -0700

    add a different solution to the question "1873. Calculate Special Bonus"

--- Advanced SQL 50/Easy/Select/1873-Calculate-Special-Bonus/solution.sql
+++ Advanced SQL 50/Easy/Select/1873-Calculate-Special-Bonus/solution.sql
@@ -1,8 +1,18 @@
--- PostgreSQL - Solution 1 - Case along with LEFT()
+-- PostgreSQL - Solution 1 - Case with LEFT()
 SELECT
     employee_id,
     CASE
         WHEN employee_id % 2 = 1 AND LEFT(name, 1) != 'M' THEN salary
+        ELSE 0
+    END AS bonus
+FROM employees
+ORDER BY 1 ASC;
+
+-- PostgreSQL - Solution 1 - Case with NOT LIKE
+SELECT
+    employee_id,
+    CASE
+        WHEN employee_id % 2 = 1 AND name NOT LIKE 'M%' THEN salary
         ELSE 0
     END AS bonus
 FROM employees
