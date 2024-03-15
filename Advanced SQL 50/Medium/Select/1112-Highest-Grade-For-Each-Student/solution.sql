26f86b527a227bf11685ffe8777208454099b423
Leo Zeng <qingchun1995@gmail.com>
2024-03-14 21:19:07 -0700

    add a solution to the question "1112. Highest Grade For Each Student"

---
+++ Advanced SQL 50/Medium/Select/1112-Highest-Grade-For-Each-Student/solution.sql
@@ -0,0 +1,17 @@
+-- PostgreSQL - Solution 1 - Window Functions
+WITH cte AS (
+    SELECT
+        *,
+        MAX(grade) OVER (PARTITION BY student_id) AS highest_grade
+    FROM enrollments
+),
+cte2 AS (
+    SELECT
+        *,
+        MIN(course_id) OVER (PARTITION BY student_id) AS smallest_course_id
+    FROM cte
+    WHERE grade = highest_grade
+)
+SELECT student_id, course_id, grade
+FROM cte2
+WHERE course_id = smallest_course_id
