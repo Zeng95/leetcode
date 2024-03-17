89a72a7ce0e456a21b481e66ba7a54e4f65147b4
Leo Zeng <qingchun1995@gmail.com>
2024-03-18 05:57:57 +0800

    add two solutions to the question "1607. Sellers With No Sales"

---
+++ Database/Advanced-SQL-50/Easy/Basic-Joins/1607-Sellers-With-No-Sales/solution.sql
@@ -0,0 +1,19 @@
+-- PostgreSQL - Solution 1 - Subquery with the EXTRACT() function
+SELECT seller_name
+FROM seller
+WHERE seller_id NOT IN (
+    SELECT seller_id
+    FROM orders
+    WHERE EXTRACT(YEAR FROM sale_date) = 2020
+)
+ORDER BY 1 ASC;
+
+-- PostgreSQL - Solution 2 - Subquery with the BETWEEN Operator
+SELECT seller_name
+FROM seller
+WHERE seller_id NOT IN (
+    SELECT seller_id
+    FROM orders
+    WHERE sale_date between '2020-01-01' AND '2020-12-31'
+)
+ORDER BY 1 ASC;
