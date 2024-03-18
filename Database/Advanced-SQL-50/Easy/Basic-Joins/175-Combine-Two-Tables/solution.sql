36cd3ab890859cd51b1058b84136f3c666e63c80
Leo Zeng <qingchun1995@gmail.com>
2024-03-18 15:49:55 +0800

    add solutions to the question "607. Sales Person"

--- Database/Advanced-SQL-50/Easy/Basic-Joins/175-Combine-Two-Tables/solution.sql
+++ Database/Advanced-SQL-50/Easy/Basic-Joins/175-Combine-Two-Tables/solution.sql
@@ -1,3 +1,4 @@
+-- PostgreSQL - Solution 1
 SELECT firstname, lastname, city, state
 FROM person p
 LEFT JOIN address a
---
+++ Database/Advanced-SQL-50/Easy/Basic-Joins/607-Sales-Person/solution.sql
@@ -0,0 +1,32 @@
+-- PostgreSQL - Solution 1 - Joining Tables and Using Exclusion with "NOT IN"
+SELECT name
+FROM salesperson
+WHERE sales_id NOT IN (
+    SELECT DISTINCT sales_id
+    FROM orders o, company c
+    WHERE o.com_id = c.com_id AND c.name = 'RED'
+);
+
+-- PostgreSQL - Solution 2 - Joining Tables(Inner Join) and Using Exclusion with "NOT IN"
+SELECT name
+FROM salesperson
+WHERE sales_id NOT IN (
+    SELECT DISTINCT sales_id
+    FROM orders o
+    JOIN company c
+    ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
+
+-- PostgreSQL - Solution 3 - Subquery and Using Exclusion with "NOT IN"
+SELECT name
+FROM salesperson
+WHERE sales_id NOT IN (
+    SELECT DISTINCT sales_id
+    FROM orders
+    WHERE com_id = (
+        SELECT com_id
+        FROM company
+        WHERE name = 'RED'
+    )
+);
