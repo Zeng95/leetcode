730ac46d3be8fa4f1c8f5473f88f5d93535a8c2b
Leo Zeng <qingchun1995@gmail.com>
2024-03-24 17:36:23 +0800

    update solutions

--- Database/Advanced-SQL-50/Easy/Select/183. Customers Who Never Order/solution.sql
+++ Database/Advanced-SQL-50/Easy/Select/183. Customers Who Never Order/solution.sql
@@ -1,14 +1,14 @@
 -- PostgreSQL - Solution 1 - Left Join
 SELECT name AS Customers
-FROM customers c
-LEFT JOIN orders o
+FROM Customers c
+LEFT JOIN Orders o
 ON c.id = o.customerId
-WHERE o.customerId IS NULL;
+WHERE o.id IS NULL;
 
 -- PostgreSQL - Solution 2 - Filtering Data with Exclusion Criteria
 SELECT name AS Customers
-FROM customers c
+FROM Customers c
 WHERE c.id NOT IN (
     SELECT customerId
-    FROM orders
+    FROM Orders
 );
