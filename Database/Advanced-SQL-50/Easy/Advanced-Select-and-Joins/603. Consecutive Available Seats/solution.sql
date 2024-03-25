9412ecf7616ce6e775e268577158e3b7816d28d2
Leo Zeng <qingchun1995@gmail.com>
2024-03-25 21:49:11 +0800

    add solutions and the description of the question "603. Consecutive Available Seats"

---
+++ Database/Advanced-SQL-50/Easy/Advanced-Select-and-Joins/603. Consecutive Available Seats/solution.sql
@@ -0,0 +1,20 @@
+-- Solution 1
+WITH cte AS (
+    SELECT
+        seat_id,
+        free,
+        LAG(free) OVER(ORDER BY seat_id) AS free_prev,
+        LEAD(free) OVER(ORDER BY seat_id) AS free_next
+    FROM Cinema
+)
+SELECT seat_id
+FROM cte
+WHERE (free != 0 AND free = free_prev) OR (free != 0 AND free = free_next)
+
+-- Solution 2
+SELECT DISTINCT a.seat_id
+FROM Cinema a
+JOIN Cinema b
+ON ABS(a.seat_id - b.seat_id) = 1
+WHERE a.free = 1 AND b.free = 1
+ORDER BY 1;
