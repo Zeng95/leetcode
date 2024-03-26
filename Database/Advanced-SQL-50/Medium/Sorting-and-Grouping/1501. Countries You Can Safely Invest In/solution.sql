511c296bb0e8a16affaf3216e404d3d8c6b6fe3e
Leo Zeng <qingchun1995@gmail.com>
2024-03-26 22:55:09 +0800

    add a solution to the question "1501. Countries You Can Safely Invest In"

---
+++ Database/Advanced-SQL-50/Medium/Sorting-and-Grouping/1501. Countries You Can Safely Invest In/solution.sql
@@ -0,0 +1,11 @@
+SELECT Country.name AS country
+FROM Person
+JOIN Calls
+ON Person.id = Calls.caller_id OR Person.id = Calls.callee_id
+JOIN Country
+ON Country.country_code = LEFT(Person.phone_number, 3)
+GROUP BY 1
+HAVING AVG(duration) > (
+    SELECT AVG(duration)
+    FROM Calls
+)
