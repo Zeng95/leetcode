3fd4b28868934ea65df7ac64b384c9efc912c308
Leo Zeng <qingchun1995@gmail.com>
2024-03-27 14:58:11 +0800

    update solution

--- Database/Advanced-SQL-50/Easy/Subqueries/512. Game Play Analysis II/solution.sql
+++ Database/Advanced-SQL-50/Easy/Subqueries/512. Game Play Analysis II/solution.sql
@@ -1,9 +1,18 @@
 -- Solution 1
 WITH cte AS (
-    SELECT player_id, MIN(event_date) AS first_login_date
+    SELECT player_id, MIN(event_date) AS event_date
     FROM Activity
     GROUP BY 1
 )
 SELECT Activity.player_id, Activity.device_id
 FROM cte, Activity
-WHERE cte.first_login_date = Activity.event_date AND cte.player_id = Activity.player_id;
+WHERE cte.event_date = Activity.event_date AND cte.player_id = Activity.player_id;
+
+-- Solution 2
+SELECT player_id, device_id
+FROM Activity
+WHERE (player_id, event_date) IN (
+    SELECT player_id, MIN(event_date) AS event_date
+    FROM Activity
+    GROUP BY 1
+);
