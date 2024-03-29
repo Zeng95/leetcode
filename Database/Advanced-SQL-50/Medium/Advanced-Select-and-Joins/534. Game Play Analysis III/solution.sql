8028a96d4ca9ba29dff0784f3d679c340a602dc9
Leo Zeng <qingchun1995@gmail.com>
2024-03-29 16:45:17 +0800

    chore: update comment

--- Database/Advanced-SQL-50/Medium/Advanced-Select-and-Joins/534. Game Play Analysis III/solution.sql
+++ Database/Advanced-SQL-50/Medium/Advanced-Select-and-Joins/534. Game Play Analysis III/solution.sql
@@ -1,4 +1,4 @@
--- Solution 1
+-- Solution 1 - Self Join
 SELECT a.player_id, a.event_date, SUM(b.games_played) AS games_played_so_far
 FROM Activity a, Activity b
 WHERE a.player_id = b.player_id AND a.event_date >= b.event_date
