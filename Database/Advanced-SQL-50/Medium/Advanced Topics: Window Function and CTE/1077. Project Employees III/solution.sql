cae91bbeb9730f73de7a97de143098d2efc59a16
Leo Zeng <qingchun1995@gmail.com>
2024-04-03 12:44:54 +0800

    add a new solution to the question "1077. Project Employees III"

--- Database/Advanced-SQL-50/Medium/Advanced Topics: Window Function and CTE/1077. Project Employees III/solution.sql
+++ Database/Advanced-SQL-50/Medium/Advanced Topics: Window Function and CTE/1077. Project Employees III/solution.sql
@@ -13,3 +13,20 @@ WITH cte AS (
 SELECT project_id, employee_id
 FROM cte
 WHERE experience_years = max_years;
+
+-- Solution 2
+WITH project_and_employee AS (
+    SELECT p.project_id, e.employee_id, e.experience_years
+    FROM Project p
+    JOIN Employee e
+    ON p.employee_id = e.employee_id
+)
+SELECT a.project_id, a.employee_id
+FROM project_and_employee a
+JOIN (
+    SELECT project_id, MAX(experience_years) AS max_years
+    FROM project_and_employee
+    GROUP BY 1
+) b
+ON a.experience_years = b.max_years
+AND a.project_id = b.project_id
