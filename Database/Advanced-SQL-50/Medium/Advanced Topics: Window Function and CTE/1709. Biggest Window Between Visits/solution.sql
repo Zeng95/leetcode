591b2e295231e1260bb9532cfe5c09914ded671b
Leo Zeng <qingchun1995@gmail.com>
2024-04-04 12:49:31 +0800

    add solutions to the question "1709. Biggest Window Between Visits"

---
+++ Database/Advanced-SQL-50/Medium/Advanced Topics: Window Function and CTE/1709. Biggest Window Between Visits/solution.sql
@@ -0,0 +1,31 @@
+-- Solution 1 - MySQL
+WITH cte AS (
+    SELECT
+        user_id,
+        visit_date,
+        CASE
+            WHEN LEAD(visit_date) OVER (PARTITION BY user_id ORDER BY visit_date) IS NULL THEN '2021-1-1'
+            ELSE LEAD(visit_date) OVER (PARTITION BY user_id ORDER BY visit_date)
+        END AS next_visit_date
+    FROM UserVisits
+    GROUP BY 1, 2
+)
+SELECT user_id, MAX(DATEDIFF(next_visit_date, visit_date)) AS biggest_window
+FROM cte
+GROUP BY 1;
+
+-- Solution 2 - PostgreSQL
+WITH cte AS (
+    SELECT
+        user_id,
+        visit_date,
+        CASE
+            WHEN LEAD(visit_date) OVER (PARTITION BY user_id ORDER BY visit_date) IS NULL THEN '2021-1-1'
+            ELSE LEAD(visit_date) OVER (PARTITION BY user_id ORDER BY visit_date)
+        END AS next_visit_date
+    FROM UserVisits
+    GROUP BY 1, 2
+)
+SELECT user_id, MAX(next_visit_date - visit_date) AS biggest_window
+FROM cte
+GROUP BY 1;
