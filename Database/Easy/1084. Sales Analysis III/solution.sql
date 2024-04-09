ab2c858ac947b448973bc1c6a25f70cf9232ddf9
Leo Zeng <qingchun1995@gmail.com>
2024-04-09 11:32:15 +0800

    chore: 1084. Sales Analysis III

---
+++ Database/Easy/1084. Sales Analysis III/solution.sql
@@ -0,0 +1,18 @@
+-- Solution 1
+SELECT DISTINCT p.product_id, p.product_name
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
+GROUP BY 1, 2
+HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31';
+
+-- Solution 2
+SELECT DISTINCT p.product_id, p.product_name
+FROM Sales s
+JOIN Product p
+ON s.product_id = p.product_id
+WHERE (s.sale_date BETWEEN '2019-01-01' AND '2019-03-31') AND p.product_id NOT IN (
+    SELECT product_id
+    FROM Sales
+    WHERE sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31'
+);
